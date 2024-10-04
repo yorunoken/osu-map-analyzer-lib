c7a18f50a9238236b0019c7041e4ddafb42b6f61
Yoru <m.f.fetvaci@hotmail.com>
2024-10-05 02:54:38 +0300

    add configs in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,42 @@ pub mod analyze;
 mod utils;
 
 pub use rosu_map;
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use crate::analyze;
+
+    #[test]
+    fn test_jump_map() {
+        let path = Path::new("example-maps/jump-flowerdance.osu");
+        let map = rosu_map::from_path::<rosu_map::Beatmap>(path).unwrap();
+
+        println!("Testing map: {} - {}", map.title, map.artist);
+
+        let mut stream_analyzer = analyze::Stream::new(map.clone());
+        let stream_analasis = stream_analyzer.analyze();
+        println!("stream: \n{:#?}", stream_analasis);
+
+        let mut jump_analyzer = analyze::Jump::new(map);
+        let jump_analasis = jump_analyzer.analyze();
+        println!("jump: \n{:#?}", jump_analasis);
+    }
+
+    #[test]
+    fn test_stream_map() {
+        let path = Path::new("example-maps/stream-honesty.osu");
+        let map = rosu_map::from_path::<rosu_map::Beatmap>(path).unwrap();
+
+        println!("Testing map: {} - {}", map.title, map.artist);
+
+        let mut stream_analyzer = analyze::Stream::new(map.clone());
+        let stream_analasis = stream_analyzer.analyze();
+        println!("stream: \n{:#?}", stream_analasis);
+
+        let mut jump_analyzer = analyze::Jump::new(map);
+        let jump_analasis = jump_analyzer.analyze();
+        println!("jump: \n{:#?}", jump_analasis);
+    }
+}
